250e14949f20e2af7ba258907561d09662314fca
Ben Harrington <harringtonben@icloud.com>
2018-03-03 09:41:19 -0600

    finished writing first failing test

---
+++ stringcalculator.test/StringCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using stringcalculator;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringcalculator.test
+{
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+        [Test]
+        public void If_I_have_an_empty_string_the_method_should_return_0()
+        {
+            var number = "";
+            var calculator = new Calculator();
+
+            var result = calculator.CalculateString(number);
+
+            Assert.AreEqual("0", result);
+        }
+    }
+}
